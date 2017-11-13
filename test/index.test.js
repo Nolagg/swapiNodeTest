aba0e61086df31cdcbb7e87ed9e8b7e4081b00c6
Nola Giltinan <ngiltinan@redfaire.com>
2017-11-13 10:11:57 +0000

    Comments ONLY

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
-const expect = require('chai').expect;
+// Lots more testing should go in here.  
+// Note to self:  incorporate nock.  See https://semaphoreci.com/community/tutorials/mocking-external-http-requests-in-node-tests-with-nock
 
+const expect = require('chai').expect;
 
 describe('First test', () => {
     it('Should assert true to be true', () => {
